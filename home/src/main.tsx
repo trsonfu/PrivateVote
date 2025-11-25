0915f11720907b6949e2754f262ca476b150a440
kyrtill6b <kyrtill6b@vivumail.com>
2025-11-25 17:31:04 +0700

    + Fix Buffer is not defined

--- home/src/main.tsx
+++ home/src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Buffer } from 'buffer'
 import './index.css'
 import './styles/neoBrutal.css'
 import App from './App.tsx'
+
+// The relayer SDK depends on Node's Buffer API; create the polyfill once here.
+if (!globalThis.Buffer) {
+  globalThis.Buffer = Buffer
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
